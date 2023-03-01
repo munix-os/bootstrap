46533c951cdbde40e485253f06190faeeafd4d06
cleanbaja <cleanbaja@protonmail.com>
2023-03-01 22:02:37 +0000

    binutils: bump version to 2.40 (and use the gold linker as default)

--- scripts/sync-tools.sh
+++ scripts/sync-tools.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 BUILD_DIR="$1"
-[ "x$BUILD_DIR" = "x" ] && BUILD_DIR="build"
-
+[ "x$BUILD_DIR" == "x" ] && BUILD_DIR="build"
 files="$(find $BUILD_DIR/tools/ -maxdepth 1 -name *.tar.gz)"
-filedir="$(mktemp -d)"
 filelist=($files)
 
-for file in "${filelist[@]}"; do
-	echo "uploading tool $(basename $file .tar.gz)"
-	cp $file $filedir
-done
+if [ "x$2" == "x" ]; then
+	filedir="$(mktemp -d)"
 
-scp $filedir/* sigterm@mkall.org:/home/sigterm/.www/tools/
+	for file in "${filelist[@]}"; do
+		echo "uploading tool $(basename $file .tar.gz)"
+		cp $file $filedir
+	done
+
+	scp $filedir/* sigterm@mkall.org:/home/sigterm/.www/tools/
+	rm -rf "$filedir"
+else
+	echo "uploading tool $2"
+	scp $BUILD_DIR/tools/host-$2.tar.gz sigterm@mkall.org:/home/sigterm/.www/tools/
+fi
